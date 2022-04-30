c3f4c1fb2b4acbe82ffe6233e68f0c3e976683cc
Ry <ryanst755@gmail.com>
2022-04-30 14:20:16 -0700

    Initial commit

---
+++ polybar/launch_polybar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top &
+    done
+else
+    polybar --reload top &
+fi
