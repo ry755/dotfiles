68f1c8ef62d8fcfb2769d9f4e6340a57dcc99c29
Ry <ryanst755@gmail.com>
2022-08-08 01:21:06 -0700

    Switch from awesome back to i3

--- awesome/configuration/init.lua
+++
@@ -1,8 +0,0 @@
-terminal = "alacritty"
-editor = os.getenv("EDITOR") or "kak"
-editor_cmd = terminal .. " -e " .. editor
-modkey = "Mod4"
-
-require("configuration.binds")
-require("configuration.layout")
-
--- awesome/configuration/layout.lua
+++
@@ -1,7 +0,0 @@
-local awful = require("awful")
-
-awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile
-}
-
